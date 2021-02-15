41f071ab07933b17302a72f924d6d8c263b34c00
bazsycus <62937986+bazsycus@users.noreply.github.com>
2021-02-15 16:53:33 +0100

    Add files via upload

---
+++ basic graphics/12_grafika_bevezeto/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace _12_grafika_bevezeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paraméter 1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(212, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(27, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 28);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Vonal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(765, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rajzol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(349, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Paraméter 2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(492, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 29);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Paraméter 3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(626, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 29);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(622, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Paraméter 4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(913, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Letöröl";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(27, 95);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(177, 28);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.Text = "Téglalap körvonal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(27, 129);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(167, 28);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.Text = "Elipszis körvonal";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(27, 163);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(152, 28);
+            this.radioButton4.TabIndex = 13;
+            this.radioButton4.Text = "Telített téglalap";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(27, 197);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(148, 28);
+            this.radioButton5.TabIndex = 14;
+            this.radioButton5.Text = "Telített elipszis";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 46);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Szín RGB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(177, 46);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Dialógus ablak";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(9, 393);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(45, 366);
+            this.trackBar1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 24);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Piros";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(92, 366);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 24);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Kék";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(89, 393);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar2.Size = new System.Drawing.Size(45, 366);
+            this.trackBar2.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(168, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 24);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Zöld";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(159, 393);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar3.Size = new System.Drawing.Size(45, 366);
+            this.trackBar3.TabIndex = 21;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(27, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 36);
+            this.panel1.TabIndex = 23;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1077, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(208, 46);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Óra végi érdekesség";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1332, 773);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Grafika bevezető";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ basic graphics/12_grafika_bevezeto/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_grafika_bevezeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Color szinAlap;
+        //Color szin osztály
+        Graphics rajzlap;
+        //grafikai elem, rá tudunk rajzolni
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            szinAlap = Color.Black;
+            panel1.BackColor = szinAlap;
+            rajzlap = this.CreateGraphics();
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int iR, iG, iB;
+            iR = trackBar1.Value;
+            iB = trackBar2.Value;
+            iG = trackBar3.Value;
+            szinAlap = Color.FromArgb(iR,iG,iB);
+            panel1.BackColor = szinAlap;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog()==DialogResult.OK)
+            {
+                szinAlap = cd.Color;
+                panel1.BackColor = cd.Color;
+                trackBar1.Value = cd.Color.R;
+                trackBar2.Value = cd.Color.B;
+                trackBar3.Value = cd.Color.G;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iP1, iP2, iP3, iP4;
+            iP1 = int.Parse(textBox1.Text);
+            iP2 = int.Parse(textBox2.Text);
+            iP3 = int.Parse(textBox3.Text);
+            iP4 = int.Parse(textBox4.Text);
+
+            Pen tollVekony = new Pen(szinAlap,2);//szín, vastagság (px)
+            SolidBrush ecsetKozos = new SolidBrush(szinAlap);
+            
+            Point pontP12 = new Point(iP1, iP2);
+            Point pontP34 = new Point(iP3, iP4);
+            Rectangle teglalapP1234 = new Rectangle(iP1, iP2, iP3,iP4);
+            if (radioButton1.Checked)//==true
+            {
+                rajzlap.DrawLine(tollVekony,iP1, iP2, iP3, iP4);//kezdőpont x-y, végpont x-y
+              //  rajzlap.DrawLine(tollVekony, pontP12, pontP34);//toll, kezdőpont végpont
+            }
+            if (radioButton2.Checked)
+            {
+                rajzlap.DrawRectangle(tollVekony, iP1, iP2, iP3, iP4);
+                //toll;balfelső X, balfelső Y, szélesség, magasság
+                //rajzlap.DrawRectangle(tollVekony, teglalapP1234);
+            }
+            if (radioButton3.Checked)
+            {
+                rajzlap.DrawEllipse(tollVekony, iP1, iP2, iP3, iP4);
+                //toll;balfelső X, balfelső Y, szélesség, magasság
+            }
+            if (radioButton4.Checked)
+            {
+                rajzlap.FillRectangle(ecsetKozos, iP1, iP2, iP3, iP4);
+                
+            }
+            if (radioButton5.Checked)
+            {
+                rajzlap.FillEllipse(ecsetKozos, iP1, iP2, iP3, iP4);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rajzlap.Clear(DefaultBackColor);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //hsz felvétele
+            Point[] pontaHsz = new Point[3];
+            pontaHsz[0] = new Point(500, 500);
+            pontaHsz[1] = new Point(700, 500);
+            pontaHsz[2] = new Point(600, 267);
+            //kp felvétele
+            Point pontUtolso = new Point(600, 443);
+
+            //Kirajzolni a pontokat
+            SolidBrush ecsetKi = new SolidBrush(szinAlap);
+
+            for (int i = 0; i <3; i++)
+            {
+                rajzlap.FillEllipse(ecsetKi, pontaHsz[i].X - 2, pontaHsz[i].Y - 2, 4, 4);
+            }
+            rajzlap.FillEllipse(ecsetKi, pontUtolso.X - 2, pontUtolso.Y - 2, 4, 4);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10000; i++)//10000 pont generálása :)
+            {
+                int iGeneralt = rnd.Next(0, 3);
+                pontUtolso = new Point((int)((pontaHsz[iGeneralt].X + pontUtolso.X) / 2),
+                    (int)((pontaHsz[iGeneralt].Y + pontUtolso.Y) / 2));
+                rajzlap.FillEllipse(ecsetKi, pontUtolso.X - 2, pontUtolso.Y - 2, 4, 4);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
